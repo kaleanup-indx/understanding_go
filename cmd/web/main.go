1802336fc225ede06fa3c9fbe60e9807f97368e8
Anup Kale <akale@intelligentdx.com>
2024-06-26 10:09:54 +0530

    feat: start implementing sessions

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/alexedwards/scs/v2"
 	"github.com/kaleanup-indx/m/v2/pkg/config"
 	"github.com/kaleanup-indx/m/v2/pkg/handlers"
 	"github.com/kaleanup-indx/m/v2/pkg/render"
@@ -12,17 +14,29 @@ import (
 
 const portNumber = ":8080"
 
+var app config.AppConfig
+var session *scs.SessionManager
+
 // main the main function of the applicaiton.
 func main() {
 
-	var app config.AppConfig
+	// change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
 		log.Fatal("cannot create template cache")
 	}
 	app.TemplateCache = tc
-	app.UseCache = false
+	app.UseCache = app.InProduction
 
 	repo := handlers.NewRepo(&app)
 	handlers.NewHandlers(repo)
@@ -38,5 +52,4 @@ func main() {
 
 	err = srv.ListenAndServe()
 	log.Fatal(err)
-
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/kaleanup-indx/m/v2
 go 1.22.4
 
 require (
+	github.com/alexedwards/scs/v2 v2.8.0 // indirect
 	github.com/go-chi/chi/v5 v5.0.14 // indirect
 	github.com/justinas/nosurf v1.1.1 // indirect
 )
